3160acbe1f60a8eb8b2a49b704bb4b2b72f83f27
fyn.ifyn <77541419+ifyyn@users.noreply.github.com>
2024-06-20 14:16:38 +0700

    Update ChatView.vue

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="flex flex-col min-h-screen p-4">
   <div class="input-user bg-gray-100 text-zinc-700 rounded-lg p-4 mb-2 text-end">
-    <h1 class="text-md font-bold">Hi</h1>
+    <h1 class="text-md font-bold">pagi</h1>
   </div>
   <div class="response-bot text-zinc-700 p-4 mb-2">
-    <h1 class="text-md font-bold">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit unde dolores delectus dicta culpa deserunt? Voluptate at aperiam alias vero.</h1>
+    <h1 class="text-md font-bold">ada yang bisa sahabatmu.ia bantu ?.</h1>
+  </div>
+    <div class="input-user bg-gray-100 text-zinc-700 rounded-lg p-4 mb-2 text-end">
+    <h1 class="text-md font-bold">bisakah kamu membantu aku,mengatasi kesehatan mental?</h1>
+  </div>
+     <div class="response-bot text-zinc-700 p-4 mb-2">
+    <h1 class="text-md font-bold">Tentu, saya akan mencoba membantu sebaik mungkin. Apa yang sedang kamu alami atau butuhkan bantuan terkait kesehatan mental?</h1>
+  </div>
+    </div>
+    <div class="input-user bg-gray-100 text-zinc-700 rounded-lg p-4 mb-2 text-end">
+    <h1 class="text-md font-bold">Hati saya gelisah</h1>
+  </div>
+   </div>
+     <div class="response-bot text-zinc-700 p-4 mb-2">
+    <h1 class="text-md font-bold">Ingatlah bahwa perasaan gelisah adalah hal yang wajar dan banyak orang mengalaminya. Penting untuk menjaga keseimbangan emosional dan mencari cara yang tepat untuk mengatasi perasaan tersebut.</h1>
   </div>
   <div class="input mt-auto flex">
     <input type="text" class="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Type your message...">
